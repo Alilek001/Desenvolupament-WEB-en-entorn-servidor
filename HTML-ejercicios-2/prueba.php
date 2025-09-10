7f003c6f93c9e067e409062125ae6a8cf96a9635
Alilek001 <alilekhal772@gmail.com>
2025-09-10 19:14:26 +0200

    ejercicio2

---
+++ HTML-ejercicios-2/prueba.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Primera prueba php</title>
+</head>
+<body>
+  Este es un archivo php que se encuentra en el servidor.
+  <?php
+    include("archivo.php");
+    include("cabecera.inc.php");
+  ?>
+</body>
+</html>
+
